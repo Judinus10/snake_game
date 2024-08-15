15211a90e9509246f3e91ed0c6d1f1ec316ab50c
judinus10 <judes.20222403@iit.ac.lk>
2024-08-15 16:31:01 +0530

    Set up the screen

---
+++ s.py
@@ -0,0 +1,15 @@
+import turtle
+
+# set up the screen 
+wn = turtle.Screen()
+wn.title("Snake Game")
+wn.bgcolor("green")
+wn.setup(width=600, height=600)
+wn.tracer(0)
+
+
+
+
+
+
+wn.mainloop() #to keep window open
